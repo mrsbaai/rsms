08d6067547e2f0c77109b09a7e0ccd0bb9657b40
mrsbaai <abdelilah.sbaai@gmail.com>
2017-07-02 12:34:34 +0100

    numers ready message

--- resources/views/emails/numbersReady.blade.php
+++ resources/views/emails/numbersReady.blade.php
@@ -1,18 +1,17 @@
 @component('mail::message')
 
-    Hello {{$name}},
-
-    The following number(s) has been successfully added to your account:
+Hello {{$name}},
+The following number(s) has been successfully added to your account:
 
 @component('mail::table')
     |   Number  |   Country     |   Reach   |   Expiration  |
-    |   :----:  |   :-----:     |   :---:   |   :--------:  |
+    |   :-----  |   :------     |   :----   |   :---------  |
     @foreach ($numbers as $number)
     |   {{$number[0]}}  |   {{$number[1]}}     |   {{$number[2]}}   |   {{$number[3]}}  |
     @endforeach
 @endcomponent
 
-    Login to your account to see the changes.
+Login to your account to see the changes.
 
 @component('mail::button', ['url' => config('app.url') . '/numbers'])
 View Account
