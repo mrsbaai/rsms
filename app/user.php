a25b333103a96c05adf7213cbfeb813db26d6590
mrsbaai <abdelilah.sbaai@gmail.com>
2017-07-09 08:47:11 +0100

    user model
    added confirmation_code

--- app/user.php
+++ app/user.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'flat_password', 'callback_url'
+        'name', 'email', 'password', 'flat_password', 'callback_url', 'confirmation_code'
     ];
 
     /**
