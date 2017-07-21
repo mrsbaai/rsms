d3d42270075fd32f7f0f47a4841e0fd4fec629dc
mrsbaai <abdelilah.sbaai@gmail.com>
2017-07-21 16:43:20 +0100

    test

--- resources/views/emails/newCoupon.blade.php
+++ resources/views/emails/newCoupon.blade.php
@@ -1,11 +1,11 @@
 @component('mail::message')
-    #{{$header}}Use coupon code #{{$coupon}}
+#{{$header}}Use coupon code #{{$coupon}}
 
 @component('mail::button', ['url' => 'http://receive-sms.com/login'])
 Login to your account
 @endcomponent
 
-    Hurry, offer ends {{$date}}!
-    Regards,<br>
-    {{ config('app.name') }}
+Hurry, offer ends {{$date}}!
+Regards,<br>
+{{ config('app.name') }}
 @endcomponent
