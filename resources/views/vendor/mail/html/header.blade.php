91565785bdd33401d85d69f515d0ac06c9b18cc7
mrsbaai <abdelilah.sbaai@gmail.com>
2017-07-23 15:13:16 +0100

    logo in email test

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}">
-            <img src="https://sms-verification.net/img/logo.png">
+            <img src="{{asset('/img/logo.png')}}">
         </a>
     </td>
 </tr>
