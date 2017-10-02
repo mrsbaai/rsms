69e3087cdd8ea5737e8e0bc1638fc2c31cb5be72
mrsbaai <abdelilah.sbaai@gmail.com>
2017-10-02 21:55:03 +0100

    faqs updated

--- resources/views/faqs.blade.php
+++ resources/views/faqs.blade.php
@@ -13,10 +13,14 @@
                 <div class="row">
 
                     <h3>Frequently Asked Questions</h3>
+                    <p><b>What is this?</b></p>
+                    <p>You will find on this website phone numbers. You can use these numbers to anonymously receive SMS messages.</p>
+                    <p><b>Can I use this with online registrations?</b></p>
+                    <p>Yes you can. (Gmail, Yahoo, PayPal...).</p>
                     <p><b>What number should I use?</b></p>
-                    <p>You can use one of the demo numbers from the home page, or buy private numbers. These numbers are international and includes the country calling code. </p>
+                    <p>You can use one of the demo numbers from the home page, or register for a free account and buy private numbers. </p>
                     <p><b>Where can I read inbound messages?</b></p>
-                    <p>For the demo phone numbers, inbound messages are shown on the home page. Inbound messages for private phone numbers are shown in a private inbox.</p>
+                    <p>For the demo phone numbers, inbound messages are shown on the home page. If you buy private numbers you need to login to your account to see inbound messages</p>
                     <p><b>How to get more information?</b></p>
                     <p>If you have any question or concerns it's simple - just <a href="/contact">Contact Us</a>.</p>
 
