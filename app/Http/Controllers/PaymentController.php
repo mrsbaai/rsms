7a1c7424f691bb0326d359319e7f418a44b661ec
mrsbaai <abdelilah.sbaai@gmail.com>
2017-10-25 15:51:07 +0100

    payza ipn log for test

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -274,7 +274,7 @@ class PaymentController extends Controller
             else
             {
                 $response = urldecode($response);
-                Log::info($response);
+                Log::info($response['ap_merchant']);
 
             }
         }
