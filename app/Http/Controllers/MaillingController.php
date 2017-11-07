262e32c8b3a4ee2ac226eba39a15daa05bb2166d
mrsbaai <abdelilah.sbaai@gmail.com>
2017-11-07 14:13:14 +0000

    removed mail from mailling controller for testing

--- app/Http/Controllers/MaillingController.php
+++ app/Http/Controllers/MaillingController.php
@@ -39,7 +39,6 @@ class MaillingController extends Controller
     }
 
     public function SendTopupEmail($user_id){
-        echo $user_id . "<br/>";
         $user = User::whereid($user_id)->first();
         $balance = $user["balance"];
         $now = Carbon::now();
@@ -56,47 +55,39 @@ class MaillingController extends Controller
                         $when = Carbon::now()->addMinutes($count);
                         switch ($diff) {
                             case 14:
-                                echo "---" . $diff . "(14)---";
-                                Mail::to($user["email"])->later($when, new topupNeeded());
+                                //Mail::to($user["email"])->later($when, new topupNeeded());
                                 break;
                             case 10:
-                                echo "---" . $diff . "(10)---";
-                                Mail::to($user["email"])->later($when, new topupNeeded());
+                                //Mail::to($user["email"])->later($when, new topupNeeded());
                                 break;
                             case 7:
-                                echo "---" . $diff . "(7)---";
-                                Mail::to($user["email"])->later($when, new topupNeeded());
+                                //Mail::to($user["email"])->later($when, new topupNeeded());
                                 break;
                             case 4:
-                                echo "---" . $diff . "(4)---";
-                                Mail::to($user["email"])->later($when, new topupNeeded());
+                                //Mail::to($user["email"])->later($when, new topupNeeded());
                                 break;
                             case 1:
-                                echo "---" . $diff . "(1)---";
-                                Mail::to($user["email"])->later($when, new topupNeeded());
+                                //Mail::to($user["email"])->later($when, new topupNeeded());
                                 break;
                             case 3:
-                                echo "---" . $diff . "(3)---";
                                 $data['name'] = $user['name'];
-                                Mail::to($user["email"])->later($when, new numberRemovalNotification($data));
+                                //Mail::to($user["email"])->later($when, new numberRemovalNotification($data));
                                 break;
                             case 5:
-                                echo "---" . $diff . "(5)---";
                                 $expiration = Carbon::now()->addDays(2);
                                 $data['subj'] = "<<Receive-SMS>> Get 30% Off Coupon!";
                                 $data['header'] = "Get a 30% Off All Your Top Ups!";
                                 $data['coupon'] = $this->RandomCoupon(30,$expiration);
                                 $data['date'] = $expiration;
-                                Mail::to($user["email"])->later($when, new newCoupon($data));
+                                //Mail::to($user["email"])->later($when, new newCoupon($data));
                                 break;
                             case 2:
-                                echo "---" . $diff . "(2)---";
                                 $expiration = Carbon::now()->addDays(2);
                                 $data['subj'] = "<<Receive-SMS>> Biggest Sell Out 50% Discount!";
                                 $data['header'] = "Get a 50% Off All Your Top Ups!";
                                 $data['coupon'] = $this->RandomCoupon(50,Carbon::now()->addDays(2));
                                 $data['date'] = $expiration;
-                                Mail::to($user["email"])->later($when, new newCoupon($data));
+                                //Mail::to($user["email"])->later($when, new newCoupon($data));
                                 break;
                         }
                     }
