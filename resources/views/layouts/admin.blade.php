64d824987625913b63a6c209423d7fbe26825c1b
mrsbaai <abdelilah.sbaai@gmail.com>
2017-12-16 16:41:14 +0000

    admin css

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+    
     <link rel="icon" type="image/png" href="{{ URL::asset('img/favicon.png') }}">
 
 
