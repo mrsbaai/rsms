fb6e74b6ea0977d02e62965d08a4a8ed1c60af85
mrsbaai <abdelilah.sbaai@gmail.com>
2017-12-16 16:29:51 +0000

    removed methodes from home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -144,19 +144,5 @@
 
 
 @section('bottom')
-    <div class="container col-sm-12">
-        <div class="container col-sm-3 text-center">
-        </div>
-        <div class="container col-sm-2 text-center">
-            <img src="../img/bitcoin.png" title="Bitcoin">
-        </div>
-        <div class="container col-sm-2 text-center">
-            <img src="../img/payza.png" title="Payza">
-        </div>
-        <div class="container col-sm-2 text-center">
-            <img src="../img/paypal.png" title="PayPal">
-        </div>
-        <div class="container col-sm-3 text-center">
-        </div>
-    </div>
+
 @stop
