7d044c8f78799ad3c7da807ed9c6bf24f4b2150b
mrsbaai <abdelilah.sbaai@gmail.com>
2017-12-17 14:11:06 +0000

    support add test

--- resources/views/admin/support.blade.php
+++ resources/views/admin/support.blade.php
@@ -25,29 +25,30 @@
 
             <div class="container-fluid no-padding ">
                 <div class="col-sm-12 no-padding ">
-                    <div class="no-more-tables">
+                    <div class="table-responsive">
                         <table class="table table-condensed">
+
                             <tbody>
                             @foreach($rows as $id => $array)
                                 <tr>
                                     @foreach($array as $content)
                                         <td>{{ $content }}</td>
                                     @endforeach
-                                        <td style="width: 300px;>
-                                            {{ Form::open(array('action' => 'adminController@sendResponse', 'id' => 'mailer-form'))}}
-                                            <input type="hidden" name="email" value="{{$array['5']}}">
-                                            <input type="hidden" name="name" value="{{$array['4']}}">
-                                            <input type="hidden" name="subject" value="{{$array['3']}}">
-                                            <input type="hidden" name="id" value="{{$array['0']}}">
-                                            <textarea id="response" type="text" name="response" class="form-control" style="width: 280px; height: 200px;"></textarea>
-                                            <br/>
-                                            <input type="submit" class="btn btn-primary btn-send " value="Send">
-                                            <a class="float:right;"><a href="/admin/support/delete/{{$array['0']}}">Delete</a></span>
-                                            {{ Form::close() }}
+                                    <td style="width: 300px;">
+                                        {{ Form::open(array('action' => 'adminController@sendResponse', 'id' => 'mailer-form'))}}
+                                        <input type="hidden" name="email" value="{{$array['5']}}">
+                                        <input type="hidden" name="name" value="{{$array['4']}}">
+                                        <input type="hidden" name="subject" value="{{$array['3']}}">
+                                        <input type="hidden" name="id" value="{{$array['0']}}">
+                                        <textarea id="response" type="text" name="response" class="form-control" style="width: 280px; height: 200px;"></textarea>
+                                        <br/>
+                                        <input type="submit" class="btn btn-primary btn-send " value="Send">
+                                        <a class="float:right;"><a href="/admin/support/delete/{{$array['0']}}">Delete</a></span>
+                                        {{ Form::close() }}
 
-                                        </td>
+                                    </td>
+
                                 </tr>
-
                             @endforeach
                             </tbody>
 
