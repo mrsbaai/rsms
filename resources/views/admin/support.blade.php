37468a96046e8d5930c7cfa599adaca95c1d06ce
mrsbaai <abdelilah.sbaai@gmail.com>
2017-12-17 14:13:56 +0000

    support add test

--- resources/views/admin/support.blade.php
+++ resources/views/admin/support.blade.php
@@ -34,13 +34,13 @@
                                     @foreach($array as $content)
                                         <td>{{ $content }}</td>
                                     @endforeach
-                                    <td style="width: 400px;">
+                                    <td style="width: 300px;">
                                         {{ Form::open(array('action' => 'adminController@sendResponse', 'id' => 'mailer-form'))}}
                                         <input type="hidden" name="email" value="{{$array['5']}}">
                                         <input type="hidden" name="name" value="{{$array['4']}}">
                                         <input type="hidden" name="subject" value="{{$array['3']}}">
                                         <input type="hidden" name="id" value="{{$array['0']}}">
-                                        <textarea id="response" type="text" name="response" class="form-control" style="width: 380px; height: 200px;"></textarea>
+                                        <textarea id="response" type="text" name="response" class="form-control" style="width: 280px; height: 200px;"></textarea>
                                         <br/>
                                         <input type="submit" class="btn btn-primary btn-send " value="Send">
                                         <a class="float:right;"><a href="/admin/support/delete/{{$array['0']}}">Delete</a></span>
