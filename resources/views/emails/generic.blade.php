eb1a0ed17c0bb8d72e1a535d5e1f1f43ec5b3acf
mrsbaai <abdelilah.sbaai@gmail.com>
2017-12-23 15:45:25 +0000

    mailler

--- resources/views/emails/generic.blade.php
+++ resources/views/emails/generic.blade.php
@@ -10,11 +10,9 @@
 <br/>
 @endif
 @if($button1)
-<br/>
 @component('mail::button', ['url' => $buttonURL1])
 {{$button1}}
 @endcomponent
-<br/>
 @endif
 @if($img1)
 <br/>
@@ -35,53 +33,39 @@
 @component('mail::button', ['url' => $buttonURL2])
 {{$button2}}
 @endcomponent
-<br/>
-
 @endif
 @if($heading3)
 <div class = "heading">
 {{$heading3}}
 </div>
-<br/>
-
 @endif
 @if($text3)
 <br/>
 {{$text3}}
 <br/>
-
 @endif
 @if($button3)
 @component('mail::button', ['url' => $buttonURL3])
 {{$button3}}
 @endcomponent
-<br/>
-
 @endif
 @if($img2)
 <br/>
 <img src="{{$img2}}"/>
 <br/>
-
 @endif
 @if($heading4)
-<br/>
-
 <div class = "heading">
 {{$heading4}}
 </div>
 <br/>
-
 @endif
 @if($text4)
 <br/>
-
 {{$text4}}
 <br/>
-
 @endif
 <br/>
-
 Regards,
 <br/>
 {{ config('app.name') }} Team.
