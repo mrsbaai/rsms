51baa0aff8d56acac10b59d63e1b8b9204b47923
mrsbaai <abdelilah.sbaai@gmail.com>
2017-12-23 14:43:41 +0000

    mailler

--- resources/views/emails/generic.blade.php
+++ resources/views/emails/generic.blade.php
@@ -5,18 +5,24 @@
         </div>
     @endif
     @if($text1)
-<br/><br/>
+
+
+
         {{$text1}}
     @endif
     @if($button1)
-<br/><br/>
+
+
+
         @component('mail::button', ['url' => $buttonURL1])
             {{$button1}}
         @endcomponent
     @endif
     @if($img1)
- <br/><br/>
- <img src="{{$img1}}"/>
+
+
+
+        <img src="{{$img1}}"/>
     @endif
     @if($heading2)
         <div class = "heading">
@@ -24,11 +30,15 @@
         </div>
     @endif
     @if($text2)
-<br/><br/>
+
+
+
         {{$text2}}
     @endif
     @if($button2)
-<br/><br/>
+
+
+
         @component('mail::button', ['url' => $buttonURL2])
             {{$button2}}
         @endcomponent
@@ -39,17 +49,23 @@
         </div>
     @endif
     @if($text3)
-<br/><br/>
+
+
+
         {{$text3}}
     @endif
     @if($button3)
-<br/><br/>
+
+
+
         @component('mail::button', ['url' => $buttonURL3])
             {{$button3}}
         @endcomponent
     @endif
     @if($img2)
-<br/><br/>
+
+
+
         <img src="{{$img2}}"/>
     @endif
     @if($heading4)
@@ -58,10 +74,14 @@
         </div>
     @endif
     @if($text4)
-<br/><br/>
+
+
+
         {{$text4}}
     @endif
-<br/><br/>
-Regards,<br>
-{{ config('app.name') }}
+
+
+
+    Regards,<br>
+    {{ config('app.name') }}
 @endcomponent
