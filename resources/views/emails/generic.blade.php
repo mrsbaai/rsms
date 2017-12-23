9af6ece8b2019cbf9702070ad41578c0179f3574
mrsbaai <abdelilah.sbaai@gmail.com>
2017-12-23 14:36:09 +0000

    mailler

--- resources/views/emails/generic.blade.php
+++ resources/views/emails/generic.blade.php
@@ -1,49 +1,70 @@
 @component('mail::message')
-
-<div class = "heading">
-{{$heading1}}
-</div>
-<br/><br/>
-{{$text1}}
-@if($button1)
-<br/><br/>
-@component('mail::button', ['url' => $buttonURL1])
-{{$button1}}
-@endcomponent
-@endif
-
-<br/><br/>
-<img src="{{$img1}}"/>
-s
-<div class = "heading">
-{{$heading2}}
-</div>
-<br/><br/>
-{{$text2}}
-<br/><br/>
-@component('mail::button', ['url' => $buttonURL2])
-    {{$button2}}
-@endcomponent
-<br/><br/>
-<img src="{{$img2}}"/>
-
-<div class = "heading">
-    {{$heading3}}
-</div>
-<br/><br/>
-{{$text3}}
-<br/><br/>
-@component('mail::button', ['url' => $buttonURL3])
-    {{$button3}}
-@endcomponent
-<br/><br/>
-<img src="{{$img2}}"/>
-
-<div class = "heading">
-    {{$heading4}}
-</div>
-<br/><br/>
-{{$text4}}
+    @if($heading1)
+        <div class = "heading">
+            {{$heading1}}
+        </div>
+    @endif
+    @if($text1)
+        <br/><br/>
+        {{$text1}}
+    @endif
+    @if($button1)
+        <br/><br/>
+        @component('mail::button', ['url' => $buttonURL1])
+            {{$button1}}
+        @endcomponent
+    @endif
+    @if($img1)
+        <br/><br/>
+        <img src="{{$img1}}"/>
+    @endif
+    @if($heading2)
+        <div class = "heading">
+            {{$heading2}}
+        </div>
+    @endif
+    @if($text2)
+        <br/><br/>
+        {{$text2}}
+    @endif
+    @if($button2)
+        <br/><br/>
+        @component('mail::button', ['url' => $buttonURL2])
+            {{$button2}}
+        @endcomponent
+    @endif
+    @if($img2)
+        <br/><br/>
+        <img src="{{$img2}}"/>
+    @endif
+    @if($heading3)
+        <div class = "heading">
+            {{$heading3}}
+        </div>
+    @endif
+    @if($text3)
+        <br/><br/>
+        {{$text3}}
+    @endif
+    @if($button3)
+        <br/><br/>
+        @component('mail::button', ['url' => $buttonURL3])
+            {{$button3}}
+        @endcomponent
+    @endif
+    @if($img3)
+        <br/><br/>
+        <img src="{{$img3}}"/>
+    @endif
+    @if($heading4)
+        <div class = "heading">
+            {{$heading4}}
+        </div>
+    @endif
+    @if($text4)
+        <br/><br/>
+        {{$text4}}
+    @endif
 <br/><br/>
 Regards,<br>
 {{ config('app.name') }}
