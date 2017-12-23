477e55368aa00cd18fb235eca68452aeb0aca4d0
mrsbaai <abdelilah.sbaai@gmail.com>
2017-12-23 14:46:28 +0000

    mailler

--- resources/views/emails/generic.blade.php
+++ resources/views/emails/generic.blade.php
@@ -1,88 +1,88 @@
 @component('mail::message')
-    @if($heading1)
-        <div class = "heading">
-            {{$heading1}}
-        </div>
-    @endif
-    @if($text1)
+@if($heading1)
+<div class = "heading">
+{{$heading1}}
+</div>
+@endif
+@if($text1)
 
 
 
-        {{$text1}}
-    @endif
-    @if($button1)
+{{$text1}}
+@endif
+@if($button1)
 
 
 
-        @component('mail::button', ['url' => $buttonURL1])
-            {{$button1}}
-        @endcomponent
-    @endif
-    @if($img1)
+@component('mail::button', ['url' => $buttonURL1])
+{{$button1}}
+@endcomponent
+@endif
+@if($img1)
 
 
 
-        <img src="{{$img1}}"/>
-    @endif
-    @if($heading2)
-        <div class = "heading">
-            {{$heading2}}
-        </div>
-    @endif
-    @if($text2)
+<img src="{{$img1}}"/>
+@endif
+@if($heading2)
+<div class = "heading">
+{{$heading2}}
+</div>
+@endif
+@if($text2)
 
 
 
-        {{$text2}}
-    @endif
-    @if($button2)
+{{$text2}}
+@endif
+@if($button2)
 
 
 
-        @component('mail::button', ['url' => $buttonURL2])
-            {{$button2}}
-        @endcomponent
-    @endif
-    @if($heading3)
-        <div class = "heading">
-            {{$heading3}}
-        </div>
-    @endif
-    @if($text3)
+@component('mail::button', ['url' => $buttonURL2])
+{{$button2}}
+@endcomponent
+@endif
+@if($heading3)
+<div class = "heading">
+{{$heading3}}
+</div>
+@endif
+@if($text3)
 
 
 
-        {{$text3}}
-    @endif
-    @if($button3)
+{{$text3}}
+@endif
+@if($button3)
 
 
 
-        @component('mail::button', ['url' => $buttonURL3])
-            {{$button3}}
-        @endcomponent
-    @endif
-    @if($img2)
+@component('mail::button', ['url' => $buttonURL3])
+{{$button3}}
+@endcomponent
+@endif
+@if($img2)
 
 
 
-        <img src="{{$img2}}"/>
-    @endif
-    @if($heading4)
-        <div class = "heading">
-            {{$heading4}}
-        </div>
-    @endif
-    @if($text4)
+<img src="{{$img2}}"/>
+@endif
+@if($heading4)
+<div class = "heading">
+{{$heading4}}
+</div>
+@endif
+@if($text4)
 
 
 
-        {{$text4}}
-    @endif
+{{$text4}}
+@endif
 
 
 
-    Regards,
-    
-    {{ config('app.name') }}
+Regards,
+
+{{ config('app.name') }}
 @endcomponent
