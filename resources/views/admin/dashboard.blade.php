ae35466feacfb9314cbae810afe9dc2e90fb8505
mrsbaai <abdelilah.sbaai@gmail.com>
2018-03-30 15:24:04 +0100

    pp notif

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,16 +9,16 @@
 @section('content')
 
     <div class="container width-fix col-sm-12">
-
+        <div class="jumbotron welcome-texture">
             <h1>Send Money</h1>
             {{ Form::open(array('action' => 'PaymentController@RedirectToPaymentInternal'))}}
 
-            <input type="text" name="amount" id="amount" placeholder="amount">
-            <input type="text" name="toemail" id="toemail" placeholder="Receiver email">
-            <button id="send" type="submit">send</button>
+            <input  class="form-control" type="text" name="amount" id="amount" placeholder="amount">
+            <input  class="form-control" type="text" name="toemail" id="toemail" placeholder="Receiver email">
+            <button  class="form-control" id="send" type="submit">send</button>
 
             {{ Form::close() }}
-
+        </div>
     </div>
 
 
