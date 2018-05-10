222bd1758a5f358733628c451089a41b7dce3249
mrsbaai <abdelilah.sbaai@gmail.com>
2018-05-10 15:17:01 +0100

    fix for ato topup

--- app/Console/Commands/removeExpired.php
+++ app/Console/Commands/removeExpired.php
@@ -87,6 +87,8 @@ class removeExpired extends Command
                 $count = $count + 2;
                 $when = Carbon::now()->addMinutes($count);
 
+                $user = User::whereemail($email)->first();
+                
                 $data['name'] = $user['name'];
                 $data['date'] = $number['expiration'];
 
