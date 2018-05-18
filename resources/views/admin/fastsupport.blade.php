938bed8f6b23ac2ec3dc96d6a294955885dab193
mrsbaai <abdelilah.sbaai@gmail.com>
2018-05-18 21:35:01 +0100

    fastsupport

--- resources/views/admin/fastsupport.blade.php
+++ resources/views/admin/fastsupport.blade.php
@@ -12,13 +12,13 @@
 
 @section('content')
     <div class="container width-fix col-sm-12">
-										<h3>
+										<p class="lead">
 										{{$date}}
 										</h3><br/>
-										<h3>
+										</p>
 										{{$subject}}
 										</h3><br/>
-										<p>
+										<p class="lead">
 										{{$message}}
 										</p>
                                         {{ Form::open(array('action' => 'adminController@sendResponse', 'id' => 'fastsupport-form'))}}
@@ -32,18 +32,18 @@
                                         <a class="float:right;"><a href="/admin/support/delete/{{$id}}">Ignore</a></span>
                                         {{ Form::close() }}
 										<br/><br/><br/>
-										<p>
-										Name:</b> {{$info[0]}}<br>
-										Email:</b> {{$info[1]}}<br>
-										Registred:</b> {{$info[6]}}<br>
-										Numbers:</b> {{$info[7]}}<br>
-										SMS Received:</b> {{$info[3]}}<br>
+										<p class="lead">
+										Name: {{$info[0]}}<br>
+										Email: {{$info[1]}}<br>
+										Registred: {{$info[6]}}<br>
+										Numbers: {{$info[7]}}<br>
+										SMS Received: {{$info[3]}}<br>
 										Total Invested: ${{$info[2]}}<br>
-										Cases:</b> {{$info[4]}}<br>
-										Topups:</b> {{$info[5]}}<br>
-										Payeer:</b> {{$info[9]}}<br>
-										PayPal:</b> {{$info[10]}}<br>
-										Supports:</b> {{$info[8]}}<br>
+										Cases: {{$info[4]}}<br>
+										Topups: {{$info[5]}}<br>
+										Payeer: {{$info[9]}}<br>
+										PayPal: {{$info[10]}}<br>
+										Supports: {{$info[8]}}<br>
 										</p>
 
     </div>
