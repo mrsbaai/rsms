4ded5abf64cda9f1a1a30d202d20a2c479dd3a36
Desk <abdelilah.sbaai@gmail.com>
2018-09-20 22:13:57 +0100

    share

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -222,7 +222,7 @@
     @if(!Auth::check())
         <div class="container col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
             <a href="/register">
-                <button class="btn btn-lg share btn-success">
+                <button class="btn btn-lg btn-success">
                     Create Your Private Inbox
                 </button>
             </a>
