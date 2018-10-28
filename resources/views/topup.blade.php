f400df55637f1b58fa7875dbd00e31097b33931c
mrsbaai <abdelilah.sbaai@gmail.com>
2018-10-28 11:27:54 +0000

    topup fix

--- resources/views/topup.blade.php
+++ resources/views/topup.blade.php
@@ -145,7 +145,7 @@
 
 
                     <div class="form-group" id="final_amount">
-						<span style="font-size:80%;">Top-Up Amount:</span><span id="top" style="float:right;">$0</span><br/>
+						<span style="font-size:70%;">Top-Up Amount:</span><span id="top" style="float:right;font-size:70%;">$0</span><br/>
                         <span>Final Amount:</span><span id="price" style="float:right;">$0</span>
                     </div>
 
