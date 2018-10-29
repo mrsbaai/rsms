a571dcdcc50346c785cca1ef676533c41dacaa7a
mrsbaai <abdelilah.sbaai@gmail.com>
2018-10-29 15:26:44 +0000

    account improvments

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Auth;
+
 
 class LoginController extends Controller
 {
@@ -36,10 +36,7 @@ class LoginController extends Controller
 	 
 	protected function redirectTo()
     {
-		if (Auth::user()->balance == 0){
-			return redirect('/topup');
-		
-		}
+
     }
     public function __construct()
     {
