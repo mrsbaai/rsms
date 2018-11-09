37d75bea309d4d622e15d6bfb763054f15aff7f9
Desk <abdelilah.sbaai@gmail.com>
2018-11-09 21:33:37 +0100

    pull

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -80,6 +80,8 @@ class userController extends Controller
                     return redirect('/inbox/b/FAO4CS4GSC' . $paid);
                 }
 				
+				User::where('email', "=", $email)->update(['ip' => Request::ip()]);
+				
 				if (Auth::user()->is_first_login == 1){
 					User::where('email', "=", $email)->update(['is_first_login' => 0]);
                     return redirect('/topup');
