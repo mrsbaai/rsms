529193b67d42322894dcb3a5029cc59cf84b59d1
mrsbaai <abdelilah.sbaai@gmail.com>
2019-12-03 14:25:29 +0100

    Update messagesController.php

--- app/Http/Controllers/messagesController.php
+++ app/Http/Controllers/messagesController.php
@@ -61,10 +61,10 @@ class messagesController extends Controller
     
 
     if(Input::has('body-plain')){$text = Input::get('body-plain');}
-    if(Input::has('to')){$toemail = Input::get('to');}
-    if(Input::has('subject')){$from = Input::get('Subject');}
+    if(Input::has('To')){$toemail = Input::get('to');}
+    if(Input::has('Subject')){$from = Input::get('Subject');}
 
-    
+
     Log::info("From: $from | To: $toemail | Message: $text");
 
     }
