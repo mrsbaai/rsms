7e16ee6dfa1ad1528c808e49f95b253516b567ef
mrsbaai <abdelilah.sbaai@gmail.com>
2019-12-20 14:24:04 +0100

    Update addTextnow.blade.php

--- resources/views/numbersadmin/addTextnow.blade.php
+++ resources/views/numbersadmin/addTextnow.blade.php
@@ -12,7 +12,7 @@
 
     <div class="container width-fix col-sm-12">
         <div class="jumbotron welcome-texture">
-            <h3>Add Numbers</h3>
+            <h3>New Textnow Number</h3>
 
             {{ Form::open(array('action' => 'adminController@doAddNumber', 'id' => 'add-number-form'))}}
             <div class="container-fluid no-padding ">
