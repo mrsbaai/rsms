4ccb5923bf61a74af1cf5c8e0122f97e4387ca4e
mrsbaai <abdelilah.sbaai@gmail.com>
2020-01-18 18:43:33 +0100

    Update adminController.php

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -119,7 +119,7 @@ class adminController extends Controller
             foreach ($data['rows'] as $row) {
                 $count = count(paymentlog::all()->where('accountId',$row[0])->where('status','Reversed'));
     
-                $data['rows'][$i][1] = $row[1] ." <b> (" . $count . " Reversed)</b>";
+                $data['rows'][$i][1] = $row[1] ."(" . $count . " Reversed)";
                 $i = $i + 1;
             }
 
