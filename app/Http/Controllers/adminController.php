172d14416dc1fb69c6ce123aa14636700e457a65
mrsbaai <abdelilah.sbaai@gmail.com>
2020-01-27 14:51:46 +0100

    Update adminController.php

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -135,7 +135,7 @@ class adminController extends Controller
             fclose($file);
         };
 
-        return Response()->download($callback, 200, $headers);
+        return Response::download($callback, 200, $headers);
 
     }
     Public function dashboard(){
