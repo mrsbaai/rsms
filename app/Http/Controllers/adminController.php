b8dc11a858580e69eb12b2a9b4500b7f6aaa009e
mrsbaai <abdelilah.sbaai@gmail.com>
2020-01-28 11:45:10 +0100

    Update adminController.php

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -113,9 +113,18 @@ class adminController extends Controller
 
     public function textnowloginsarray(){
 
-        $value = number::all()->where("network", "textnow")->where("is_private", true)->sortByDesc('last_checked')->pluck('network_password', 'network_login')->toJson();
+        $value = number::all()->where("network", "textnow")->where("is_private", true)->sortByDesc('last_checked')->pluck('network_password', 'network_login');
+        return Excel::create('Filename', function($excel) use ($value) {
+    
+            $excel->setTitle('TitleBandai');
+            $excel->sheet('FirstSheet', function($sheet) use($value) {
+                $sheet->fromArray($value);
+            });
 
-        return view('admin.fkat')->with('value',$value);
+    
+        })->export('csv');
+
+        
     }
     Public function dashboard(){
 
