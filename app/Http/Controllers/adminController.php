8a9b77acc25118a2f4f3c71d38cc56a13c532e7a
mrsbaai <abdelilah.sbaai@gmail.com>
2020-01-28 13:01:39 +0100

    Update adminController.php

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -131,10 +131,13 @@ class adminController extends Controller
 
         $results = number::all()->where("network", "textnow")->where("is_private", true)->sortByDesc('last_checked')->pluck('network_password', 'network_login')->toArray();
 
-foreach ($results as $user=>$pass) {
-    echo $user . " -> ". $pass . "<br>";
-  }
-       //return response($this->array2csv($results))
+        $logins = array();
+
+        foreach ($results as $user=>$pass) {
+        array_push($logins, array( $user , $pass));
+
+        }
+       return response($this->array2csv($logins))
 
        ;
         //return view('admin.flat')->with('value',$value);
