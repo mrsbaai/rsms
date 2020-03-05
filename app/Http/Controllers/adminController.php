f4591720fea674fcb1d6aa8b0772ab28ead2169c
mrsbaai <abdelilah.sbaai@gmail.com>
2020-03-05 10:49:51 +0100

    vv

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -1007,7 +1007,34 @@ public function testMacro(){
 
 }
 
+public function textnowverify(){
 
+    Log::info($_REQUEST);
+
+
+    Log::info(Input::get('body-plain'));
+
+    $url = "google.com";
+         
+    $curlSession = curl_init();
+
+
+    curl_setopt($curlSession, CURLOPT_URL, $url);
+
+    curl_setopt($curlSession, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curlSession, CURLOPT_FOLLOWLOCATION, 1); // allow redirects
+    curl_setopt($curlSession, CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($curlSession, CURLOPT_MAXREDIRS,5); // return into a variable
+
+    curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+
+    $ret = curl_exec($curlSession);
+
+
+    curl_close($curlSession);
+
+}
 public function runMacro($isFirst = false){
 
     $lines = macro::all()
