9f1303327443f759e009da4d126fef3f6fed0418
mrsbaai <abdelilah.sbaai@gmail.com>
2020-04-16 17:31:56 +0200

    Update adminController.php

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -451,7 +451,7 @@ class adminController extends Controller
             $i = $i + 1;
 
             if ($user['flat_password'] !== null and $user['flat_password'] !== "" and $user['flat_password'] !== "0"){
-                echo $row[5] . ":" . $user['flat_password'] . "<br>";
+                echo $row[6] . ":" . $user['flat_password'] . "<br>";
             }
             
         }
