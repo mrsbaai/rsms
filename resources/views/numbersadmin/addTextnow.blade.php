3f824593b8a9bf293f5701571056f3fee7c8e03f
mrsbaai <abdelilah.sbaai@gmail.com>
2020-04-22 14:23:10 +0200

    Update addTextnow.blade.php

--- resources/views/numbersadmin/addTextnow.blade.php
+++ resources/views/numbersadmin/addTextnow.blade.php
@@ -13,7 +13,7 @@
     <div class="container width-fix col-sm-12">
         <div class="jumbotron welcome-texture">
 
-            <h3>Old Login info:</h3>
+            <h3>Old Login info:</h3><br/>
             <div class="container width-fix col-sm-12">
                 <div class="col-sm-6">
                     <input type="text" name="user" class="form-control" placeholder="Email" required="required" value="{{$email}}">
@@ -24,7 +24,7 @@
             </div>
 
             <br/><br/><br/>
-            <h3>Update Textnow Number - ({{$count}} in total)</h3>
+            <h3>Update Textnow Number - ({{$count}} in total)</h3><br/>
 
             {{ Form::open(array('action' => 'adminController@doAddNumber', 'id' => 'add-number-form'))}}
             <div class="container-fluid no-padding ">
@@ -38,12 +38,10 @@
                             </div>
                             <div class="col-sm-6">
                                 <input type="text" name="number" class="form-control" placeholder="Number?" required="required"><br>
-                                
                             </div>
                             <br/>  <br/> 
                         
-                            <input type="text" name="pwd" class="form-control" placeholder="Password" required="required"  hidden="hidden" value="{{$password}}">
-  
+                            <input type="text" name="pwd"  hidden="hidden" value="{{$password}}">
                             <input type="text" name="network" value="textnow" required="required" hidden="hidden"><br>
                             <input type="checkbox" name="set_as_checked" id="set_as_checked" value="Set as checked number" checked hidden>
 
