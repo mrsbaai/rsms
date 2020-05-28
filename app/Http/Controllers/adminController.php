26a023a0a6413de6821519e9ee14e19ef57f3e1d
mrsbaai <abdelilah.sbaai@gmail.com>
2020-05-28 18:48:28 +0200

    Update adminController.php

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -764,7 +764,7 @@ class adminController extends Controller
     
     public function updatenumber($id, $num, $user, $pwd, $ret, $ret2){
 echo $id . "<br/>" . $num . "<br/>" . $user . "<br/>" . $pwd . "<br/>" . $ret . "<br/>" . $ret2;
-        if ($ret == " Your account information was successfully changed. " and $ret2 == $user){
+        if ($ret == "Your account information was successfully changed." and $ret2 == $user){
             str_replace("%20", "", $num);
             $num = preg_replace('/[^0-9]/', '', $num);
 
