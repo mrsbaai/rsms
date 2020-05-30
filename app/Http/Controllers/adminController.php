6775d4ef8bfd9dd6d164401d9b94e3facd867e92
mrsbaai <abdelilah.sbaai@gmail.com>
2020-05-30 15:16:27 +0200

    Update adminController.php

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -1228,7 +1228,7 @@ public function updateNumbersMacro($stage="login",$id=null,$ret=null, $fix1=null
         array_push($macro, 'WAIT SECONDS=25');
         array_push($macro, 'SET !VAR1 {{!URLCURRENT}}');
         array_push($macro, 'TAG POS=1 TYPE=SPAN ATTR=CLASS:*uikit-text--danger EXTRACT=txt');
-        array_push($macro, 'SET !EXTRACT EVAL("\"{{!EXTRACT}}\".replace(\"#EANF#\", \"\");")');
+        array_push($macro, 'SET !EXTRACT EVAL(""{{!EXTRACT}}".replace("#EANF#", "");")');
         array_push($macro, 'TAB CLOSEALLOTHERS');
         array_push($macro, 'TAB OPEN');
         array_push($macro, 'TAB T=2');
