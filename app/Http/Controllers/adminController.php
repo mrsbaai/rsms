f39dbcf73fc2790a6a25a48ea1ff9b38ec88d72b
mrsbaai <abdelilah.sbaai@gmail.com>
2020-07-23 19:25:53 +0100

    fix

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -545,11 +545,10 @@ class adminController extends Controller
         $count_free = number::where('network', 'textnow')->where('network_login', 'not like', 'aa@%')->where('email', null)->where('is_private', true)->where('info', "<>", "no")->where('last_checked', '>', Carbon::now()->subDays(5)->toDateTimeString())->count();
         $Simplepush->send("W6T4J9", "Available numbers", "Available numbers count: " . $count_free, "Available numbers");
 
-        return number::all()->where('is_private',true)->where('is_active',true)->where('email', null)->where('info', "<>", "no")->sortBydesc('last_checked')->first();
-
+   
         $demoNumbers = number::all()->where('is_private',false)->where('is_active',true)->sortBydesc('last_checked');
         foreach ($demoNumbers as $demoNumber) {
-            if ($demoNumber['last_checked'] < Carbon::now()->subMinutes(120)){
+            if ($demoNumber['last_checked'] < Carbon::now()->subMinutes(330)){
                 $count_free = number::where('network', 'textnow')->where('network_login', 'not like', 'aa@%')->where('email', null)->where('is_private', true)->where('info', "<>", "no")->where('last_checked', '>', Carbon::now()->subDays(5)->toDateTimeString())->count();
                 if ($count_free > 1){
                     number::where('id', $demoNumber['id'])->update(['is_private' => true]);
