38c565d5eecd9051179c1aabe855aaab9706a98d
mrsbaai <abdelilah.sbaai@gmail.com>
2020-07-24 17:24:07 +0100

    Update adminController.php

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -559,7 +559,7 @@ class adminController extends Controller
                 if ($count_free > 1){
                     number::where('id', $demoNumber['id'])->update(['is_private' => true]);
 
-                    $newNumber = number::where('info', null)->where('network_login', 'not like', 'aa@%')->where('email', null)->where('is_private', true)->where('is_active', true)->sortBydesc('last_checked')->first();
+                    $newNumber = number::all()->where('network_login', 'not like', 'aa@%')->where('info', null)->where('email', null)->where('is_private', true)->where('is_active', true)->sortBydesc('last_checked')->first();
                     return  $newNumber;
             
                     $expiration = Carbon::now()->addMonth(20)->addDays(10);  
