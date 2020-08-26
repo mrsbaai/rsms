b9b19351459bd120e63fdc6c59bba14aa46bd255
mrsbaai <abdelilah.sbaai@gmail.com>
2020-08-26 14:42:42 +0100

    Update adminController.php

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -1288,7 +1288,7 @@ public function updateNumbersMacro($stage="login",$id=null,$ret=null, $fix1=null
         array_push($macro, 'WAIT SECONDS=2'); 
         array_push($macro, 'SET !ENCRYPTION NO'); 
         array_push($macro, 'EVENT TYPE=CLICK SELECTOR="#btn-login" BUTTON=0'); 
-        array_push($macro, 'WAIT SECONDS=120');
+        array_push($macro, 'WAIT SECONDS=20');
         array_push($macro, 'SET !VAR1 {{!URLCURRENT}}');
         array_push($macro, 'SET !VAR1 EVAL("\"{{!VAR1}}\".split(\"/\").join(\"\");")');
         array_push($macro, 'TAG POS=1 TYPE=SPAN ATTR=CLASS:*uikit-text--danger EXTRACT=txt');
@@ -1319,7 +1319,7 @@ public function updateNumbersMacro($stage="login",$id=null,$ret=null, $fix1=null
             array_push($macro, 'EVENTS TYPE=KEYPRESS SELECTOR="HTML>BODY>DIV:nth-of-type(5)>DIV>DIV>DIV:nth-of-type(2)>DIV>DIV>DIV>FORM>DIV>INPUT" CHARS="' . rand(200,800) . '"');
             array_push($macro, 'EVENT TYPE=KEYPRESS SELECTOR="#enterAreaCodeForm>DIV>INPUT" KEY=39');
             array_push($macro, 'EVENT TYPE=CLICK SELECTOR="HTML>BODY>DIV:nth-of-type(5)>DIV>DIV>DIV:nth-of-type(2)>DIV>DIV>DIV>FORM>DIV:nth-of-type(2)>INPUT" BUTTON=0');
-            array_push($macro, 'SET !TIMEOUT_STEP 120');
+            array_push($macro, 'SET !TIMEOUT_STEP 20');
             array_push($macro, 'TAG POS=1 TYPE=DIV ATTR=CLASS:"*antigate_solver*solved*"');
         
 
