39347e039ad17736972e11f7f68071ed4e87d466
mrsbaai <abdelilah.sbaai@gmail.com>
2020-11-04 16:51:04 +0100

    Update messagesController.php

--- app/Http/Controllers/messagesController.php
+++ app/Http/Controllers/messagesController.php
@@ -218,18 +218,15 @@ class messagesController extends Controller
         $curl = curl_init();
 
         curl_setopt_array($curl, [
-            CURLOPT_URL => "https://rapidapi.p.rapidapi.com/send-verification-code?phoneNumber=16194928209&brand=codecamp",
+            CURLOPT_URL => "https://api.nexmo.com/verify/json?api_key=0b8c3e63&api_secret=BDYrXLxn3SH4BtA1&number=212700124156&brand=codecamp&code_length=4",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_HTTPHEADER => [
-                "x-rapidapi-host: nexmo-nexmo-sms-verify-v1.p.rapidapi.com",
-                "x-rapidapi-key: 8876c93fb4mshaea91f14dc48babp15e86ajsn39c583358f45"
-            ],
+            CURLOPT_CUSTOMREQUEST => "GET",
+   
         ]);
         
         $response = curl_exec($curl);
