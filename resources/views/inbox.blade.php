3cb2c02e82cc0c4c0d75c9bb09013dc76460d10a
mrsbaai <abdelilah.sbaai@gmail.com>
2020-12-13 01:16:08 +0100

    Update inbox.blade.php

--- resources/views/inbox.blade.php
+++ resources/views/inbox.blade.php
@@ -30,7 +30,7 @@
 
         <div class="container width-fix col-sm-12">
             <div class="jumbotron welcome-texture">
-                <div class=" col-sm-9 no-padding"> <h3>[{{$current}}] Inbound Messages{{$count}}  - <span class="text-success">Live</span>{{$replace}}</h3></div>
+                <div class=" col-sm-9 no-padding"> <h3>[{{$current}}] Inbound Messages{{$count}}  - <span class="text-success">Live</span>{!$replace!}</h3></div>
                 <div class=" col-sm-3 no-padding" style="padding-top: 18px;">
                     <div class="col-sm-12 no-padding">
                         <select  class="form-control" id="number" onchange="go()">
