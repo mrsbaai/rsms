d89df2423342a239c75462a9ff32370fca5ddf0a
mrsbaai <abdelilah.sbaai@gmail.com>
2020-12-15 14:54:17 +0100

    Update messagesController.php

--- app/Http/Controllers/messagesController.php
+++ app/Http/Controllers/messagesController.php
@@ -156,25 +156,7 @@ class messagesController extends Controller
                     Log::info($link->href);
                 }
     
-        
-                Log::info($url);
-                $curlSession = curl_init();
-            
-            
-                curl_setopt($curlSession, CURLOPT_URL, $url);
-            
-                curl_setopt($curlSession, CURLOPT_SSL_VERIFYHOST, false);
-                curl_setopt($curlSession, CURLOPT_FOLLOWLOCATION, 1); // allow redirects
-                curl_setopt($curlSession, CURLOPT_RETURNTRANSFER,1);
-                curl_setopt($curlSession, CURLOPT_MAXREDIRS,5); // return into a variable
-            
-                curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
-                curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
-            
-                $ret = curl_exec($curlSession);
-            
-            
-                curl_close($curlSession);
+
                 return;
             }
             
