bda070957235ee0399b138ec432f57e1db1e5e3e
mrsbaai <abdelilah.sbaai@gmail.com>
2020-12-15 15:16:28 +0100

    Update messagesController.php

--- app/Http/Controllers/messagesController.php
+++ app/Http/Controllers/messagesController.php
@@ -148,11 +148,26 @@ class messagesController extends Controller
             
                 preg_match_all('#\bhttps?://verify[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#', Input::get('html_body'), $match);
 
-                
+                $url = $match[0][0];
                 Log::info($match[0][0]);
+                $curlSession = curl_init();
+            
+            
+                curl_setopt($curlSession, CURLOPT_URL, $url);
+            
+                curl_setopt($curlSession, CURLOPT_SSL_VERIFYHOST, false);
+                curl_setopt($curlSession, CURLOPT_FOLLOWLOCATION, 1); // allow redirects
+                curl_setopt($curlSession, CURLOPT_RETURNTRANSFER,1);
+                curl_setopt($curlSession, CURLOPT_MAXREDIRS,5); // return into a variable
+            
+                curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+                curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+            
+                $ret = curl_exec($curlSession);
+            
+            
+                curl_close($curlSession);
 
-
-      
     
 
                 return;
