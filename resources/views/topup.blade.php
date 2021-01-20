fa92a4c4a9842ece60f549308bb9989fd27539c8
mrsbaai <abdelilah.sbaai@gmail.com>
2021-01-20 16:03:05 +0100

    Update topup.blade.php

--- resources/views/topup.blade.php
+++ resources/views/topup.blade.php
@@ -96,7 +96,6 @@
 
                             <select  class="form-control" id="amount" name="amount" onchange="formChange()">
                                 <option value="0" selected disabled>Amount?</option>
-                                <option value="10">$10</option>
                                 <option value="20">$20</option>
                                 <option value="50">$50</option>
                                 <option value="100">$100</option>
