81f9d877fe1e6f7fa6e22cde58ae65a8562671df
mrsbaai <abdelilah.sbaai@gmail.com>
2021-01-30 15:08:21 +0100

    Update topup.blade.php

--- resources/views/topup.blade.php
+++ resources/views/topup.blade.php
@@ -165,7 +165,7 @@
 
                     <div class="form-group">
                         <div class="col-12 text-center">
-                            <span style="font-size: 50%;"><b>BITCOIN-FOREVER-456</b> (Get 10% discount with cryptocurrency payments)</span>
+                            <span style="font-size: 50%;">Use this promo code: <b>BITCOIN-FOREVER-456</b> to Get 10% discount with cryptocurrency payments. <br/> (Limited time offer)</span>
                         </div>
                     </div>
 
