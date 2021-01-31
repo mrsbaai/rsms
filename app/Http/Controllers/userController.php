4e0c94ad9c2add4839688e24685be97c23729868
mrsbaai <abdelilah.sbaai@gmail.com>
2021-01-31 15:56:40 +0100

    Update userController.php

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -154,9 +154,7 @@ class userController extends Controller
 
                 if ($p){
                     if ($p->paymentSystemId == 4){
-                        flash('<span style="font-size: 80%"><a href="https://www.coinpayments.net/feedback">Please leave a positive feedback at coinpayments.net</a></span><span style="font-family:arial">     &#128591; &#x1F64F;</span> <i class="icon-foo">&#xf015;</i> <i class="fa">
-                        &#xf000;
-                     </i>
+                        flash('<span style="font-size: 80%"><a href="https://www.coinpayments.net/feedback">Please leave a positive feedback at coinpayments.net</a></span><i class="fa">&#x1F64F;</i>
                         ')->success()->important();
 
                     }
