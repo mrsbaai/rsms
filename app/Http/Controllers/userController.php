e814d87f2561581923408086788c17020c5268b6
mrsbaai <abdelilah.sbaai@gmail.com>
2021-01-31 15:19:43 +0100

    Update userController.php

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -154,7 +154,7 @@ class userController extends Controller
 
                 if ($p){
                     if ($p->paymentSystemId == 4){
-                        flash('<span style="font-size: 80%"><a href="https://gocps.net/2ck2ls0skukdcnx2yl46m668/">Please leave a positive feedback at coinpayments.net</a></span>')->success()->important();
+                        flash('<span style="font-size: 80%"><a href="https://www.coinpayments.net/feedback">🙏 Please leave a positive feedback at coinpayments.net 🙏</a></span>')->success()->important();
 
                     }
                 }
