26449bdbf38bbeb03c17cd6085f137e1115f8dfa
mrsbaai <abdelilah.sbaai@gmail.com>
2021-02-10 15:15:20 +0100

    Update pricing.blade.php

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -37,7 +37,7 @@
 
     <div class="container col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
         <h2 style="color:white;">
-            <a style="color:white; " href="https://sms-verification.net"  target="_blank" title="SMS Verification">Interested In 200+ Numbers? Checkout SMS-Verification.net</a>
+            <a style="color:white; " href="https://bulk-pva.com/"  target="_blank" title="SMS Verification">Interested In 200+ Numbers? Checkout SMS-Verification.net</a>
         </h2>
     </div>
     <br><br>
