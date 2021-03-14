55685cca23453f1a21a65f675ab504814af1900f
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-14 15:13:42 +0100

    Update DemoNumbers.php

--- app/Console/Commands/DemoNumbers.php
+++ app/Console/Commands/DemoNumbers.php
@@ -37,7 +37,74 @@ class DemoNumbers extends Command
      */
     public function handle()
     {
-        echo 'test-----------------------------------------';
-        return;
+        echo "test2____________________--------";
+
+        $count_free = number::where('info', null)->where('network_login', 'not like', 'aa@%')->where('email', null)->where('is_private', true)->where('is_active', true)->where('last_checked', '>', Carbon::now()->subDays(5)->toDateTimeString())->count();
+        $demoNumbers = number::all()->where('is_private',false)->where('is_active',true)->sortBydesc('last_checked');
+     
+        foreach ($demoNumbers as $demoNumber) {
+
+
+                $count_free = number::where('info', null)->where('network_login', 'not like', 'aa@%')->where('email', null)->where('is_private', true)->where('is_active', true)->where('last_checked', '>', Carbon::now()->subDays(5)->toDateTimeString())->count();
+ 
+            if ($count_free > 1){
+                    $newNumber = number::where('info', null)->where('network_login', 'not like', 'aa@%')->where('email', null)->where('is_private', true)->where('is_active', true)->where('last_checked', '>', Carbon::now()->subDays(5)->toDateTimeString())->first();
+                    $count_free = $count_free -1;
+            
+                    $expiration = Carbon::now()->addMonth(20)->addDays(10);  
+                        //number::where('id', '=', $newNumber['id'])->update(['is_private' => false]);
+                        //number::where('id', '=', $newNumber['id'])->update(['expiration' => $expiration]);
+                        //message::where('receiver', $newNumber['number'])->delete();
+                        //number::where('id', $demoNumber['id'])->update(['is_active' => false]);
+                        //number::where('id', $demoNumber['id'])->update(['is_private' => true]);
+
+                    $numbers = number::all()->where('is_private',false)->where('is_active',true);
+                    $data['numbers'] = array();
+                    foreach ($numbers as $number) {
+                        $number = number::where('id', '=', $number['id'])->first();
+                        $addedNumber = array($number['number'],$number['country'],"International");          
+                        array_push($data['numbers'],$addedNumber);
+                    }
+
+
+                    print_r( $data['numbers']);
+
+
+
+                    $plucked = suppression::all()->pluck('email');
+                    $suppression =  $plucked->all();
+                    $list = subscriber::all()->where("confirmed","=",true)->pluck('email');
+                    $list = array_diff($list, $suppression);
+
+                    $emails = array();
+                    foreach($list as $email){
+                        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+                            $filterd_emails[] = $email;
+                        }
+                    }
+            
+                    //test
+                    $data['email'] = "abdelilah.sbaai@gmail.com";
+                    Mail::to($data['email'])->queue(new newdemonumbers($data));
+                        
+                                            
+                    foreach($emails as $email) {
+                        $data['email'] = $email;
+                        //send an email to subscriber
+                        //Mail::to($email)->queue(new newdemonumbers($data));
+                        
+                        
+                        
+            
+
+                    }
+                    
+                   
+                          
+                        
+                }
+            
+        }
+
     }
 }
