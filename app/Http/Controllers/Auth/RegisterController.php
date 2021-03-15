39037ccc5244ea9f6326262f13bece1d5b25a089
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-15 11:16:46 +0100

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -103,7 +103,10 @@ class RegisterController extends Controller
             $source = null;
         }
 
+        flash()->overlay('Machakil', 'la waaalo!');
 
+
+        return false;
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
