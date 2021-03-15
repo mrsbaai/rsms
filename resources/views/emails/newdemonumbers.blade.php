8afc8074b640e676b55de8ccd72176d3ca967ad2
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-15 13:44:04 +0100

    Update newdemonumbers.blade.php

--- resources/views/emails/newdemonumbers.blade.php
+++ resources/views/emails/newdemonumbers.blade.php
@@ -25,6 +25,9 @@ Cheers,<br>
 Abe
 {{ config('app.name') }} Admin
 
-<a style="font-size: 90%;" href="https://receive-sms.com/unsubscribe/{{$email}}">Unsubscribe</a>
+<div style="text-align: center;">
+    <a style="font-size: 90%;" href="https://receive-sms.com/unsubscribe/{{$email}}">Unsubscribe</a>
+</div>
+
 @endcomponent
 
