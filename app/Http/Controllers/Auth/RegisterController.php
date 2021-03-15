a3bc15e81a94c6f1f7ce08fec0f3a81e4244a4e8
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-15 11:53:40 +0100

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -103,6 +103,8 @@ class RegisterController extends Controller
             $source = null;
         }
 
+        return false;
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
