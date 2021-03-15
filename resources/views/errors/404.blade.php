b39ac14388d69b6642ead7dde7e6e96687893490
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-15 11:48:06 +0100

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,19 +10,15 @@
 
     <div class="container width-fix col-sm-12">
         <div class="jumbotron welcome-texture">
-            <div class="row">
-                <h2>You Broke The Balance Of The Internet!!</h2>
-                What do you wanna do now? <br/><br/>
+            <div class="row text-center">
+                <h1>Oh Great!!</h1>
+                <br/>
+                You broke the internet.Thanks. Thanks alot.<br/>
+                You know, we're all for mash-ups but when it comes to file names you can't just go around making things up all willy-nilly.<br/>
+                <br/><br/>
+            </a href="/">Reboot the internet</a>
+                <br/><br/>
 
-                <ul class="text-uppercase">
-                    <li><a href="/">Go home</a></li>
-                    <li><a href="{{ url('/register') }}">Register a new account</a></li>
-                    <li><a href="/pricing">Check out the pricing</a></li>
-                    <li><a href="/contact">Contact us</a></li>
-                    <li><a href="/faqs">See the frequently asked questions</a></li>
-                    <li><a href="{{ url('/api') }}">Learn about our Api service</a></li>
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                </ul>
 
 
 
