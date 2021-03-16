b392442bb173c63e7980d2e8b587a08e225bcbe0
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-16 15:40:37 +0100

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,9 +7,9 @@
 
 @section('content')
 
-<div class="container col-sm-4">
+<div class="container col-sm-3">
 </div>
-    <div class="container col-sm-4">
+    <div class="container col-sm-6">
         <div class="jumbotron welcome-texture">
             <div class="row text-center">
                 <div class="row text-center">
@@ -18,7 +18,9 @@
                 <br/>
                 <h2>Oh Great!! You broke the internet.Thanks. Thanks alot.</h2>
                 <br/>
-                You know, we're all for mash-ups but when it comes to file names you can't just go around making things up all willy-nilly.<br/>
+                You know, we're all for mash-ups <br/>
+                but when it comes to file names <br/>
+                you can't just go around making things up all willy-nilly.<br/>
                 <br/><br/>
                 
             <a href="/">Reboot the internet</a>
@@ -34,7 +36,7 @@
 
     </div>
 
-    <div class="container col-sm-4">
+    <div class="container col-sm-3">
     </div>
 
 
