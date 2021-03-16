6a6920de9ad20d370a4ee9d405ed219cb563e32e
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-16 15:58:17 +0100

    Update newdemonumbers.php

--- app/Mail/newdemonumbers.php
+++ app/Mail/newdemonumbers.php
@@ -34,7 +34,7 @@ class newdemonumbers extends Mailable
             ->subject('Fresh Demo Numbers Live!')
             ->with([
                 'numbers' => $this->numbers,
-                'email' => "",
+                'email' => $this->to[0]['address'],
             ]);
     }
 }
