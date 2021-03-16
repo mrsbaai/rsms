01509655a265eae2859137cc2a51bb804b1d23f4
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-16 15:32:41 +0100

    Update DemoNumbers.php

--- app/Console/Commands/DemoNumbers.php
+++ app/Console/Commands/DemoNumbers.php
@@ -86,7 +86,7 @@ class DemoNumbers extends Command
 
         $plucked = suppression::all()->pluck('email');
         $suppression =  $plucked->all();
-        $list = subscriber::all()->where("confirmed","=",true)->pluck('email');
+        $list = subscriber::all()->where("confirmed","=",true)->orderBy('id', 'desc')->pluck('email');
         $list =  $list->all();
 
         $list = array_diff($list, $suppression);
