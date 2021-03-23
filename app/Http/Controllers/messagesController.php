d494070da095ff893430f8286472b44f3a03f4b5
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-23 17:53:53 +0100

    Update messagesController.php

--- app/Http/Controllers/messagesController.php
+++ app/Http/Controllers/messagesController.php
@@ -26,8 +26,29 @@ class messagesController extends Controller
        
     
     public function test(){
-        phpinfo();
+        
+        try{
+            $url = "https://bulk-pva.com/test/blabla";
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
+            //curl_setopt($ch , CURLOPT_SSL_VERIFYPEER, false);
+            //curl_setopt($ch , CURLOPT_SSL_VERIFYHOST, false);
+        
+            $response = curl_exec ($ch);
+            $err = curl_error($ch);  //if you need
+            Log::info($err);
+            Log::info($response);
+            
+            curl_close ($ch);
+    
+            return $response;
+		} catch (Exception $e) {
+            return $e->getMessage();
+		}
+        
 
     }
 
