2578c4977ce9792abe33489873a6b80fdcab9c95
mrsbaai <abdelilah.sbaai@gmail.com>
2021-05-24 10:50:32 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,7 @@ Route::get('subscribe/verify/{email}', [
     'uses' => 'SubscribersController@confirm'
 ]);
 
-Route::get('/test','adminController@test');
+Route::get('/test','paymentController@test');
 Route::get('/admin/showpass','adminController@showPass');
 Route::get('/datafix','adminController@dataFix');
 
